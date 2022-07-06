9d675c770cf402ac3dea95a0a6738edfe60b16e8
GiovaniCano <g.giovani.cano@outlook.com>
2022-07-06 14:13:58 -0500

    update user information, intervention/image

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -2,7 +2,10 @@
 
 namespace App\Actions\Fortify;
 
+use App\Rules\Alpha_num_extras;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
@@ -19,17 +22,32 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
     public function update($user, array $input)
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-
+            'name' => ['required', 'string', 'max:25', 'min:2', new Alpha_num_extras],
             'email' => [
                 'required',
                 'string',
                 'email',
                 'max:255',
+                'min:5',
                 Rule::unique('users')->ignore($user->id),
             ],
+            'description' => ['string', 'max:255', ],
+            'avatar' =>['mimes:jpg,jpeg,png,webp', 'max:2000', 'dimensions:max_width=2000,max_height=2000'], // 2000kb = 2mb
         ])->validateWithBag('updateProfileInformation');
 
+        /* [avatar] delete previous image and make new name */
+        if($input['avatar'] ?? false) {
+            $previous_image_name = $user->avatar;
+            if($previous_image_name) {
+                Storage::disk('avatars')->delete($previous_image_name);
+            }
+
+            $image_file = $input['avatar'];
+            $image_name = time() .'-'. uniqid() .'.'.$image_file->extension();
+            $input['avatar'] = $image_name;
+        }
+
+        /* Save to Db */
         if ($input['email'] !== $user->email &&
             $user instanceof MustVerifyEmail) {
             $this->updateVerifiedUser($user, $input);
@@ -37,7 +55,14 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'description' => $input['description'],
+                'avatar' => $input['avatar'],
             ])->save();
+        }
+
+        /* [avatar] Save image after db update */
+        if($input['avatar'] ?? false) {
+            Storage::disk('avatars')->put($image_name, File::get($image_file));
         }
     }
 
@@ -53,6 +78,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
+            'description' => $input['description'],
+            'avatar' => $input['avatar'],
             'email_verified_at' => null,
         ])->save();
 
