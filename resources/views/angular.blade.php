b14089254e14a889a6a4d2502221a419adfe9a52
GiovaniCano <g.giovani.cano@outlook.com>
2022-07-23 00:47:54 -0500

    Angular and Heroku

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html><html lang="en"><head>
+  <meta charset="utf-8">
+  <title>Angular Blog</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<style>body{font-family:Helvetica,Arial,sans-serif;letter-spacing:.6px}html{font-size:62.5%;box-sizing:border-box}body{font-size:16px;margin:0;background-color:#d7dce5}*,*:before,*:after{box-sizing:inherit}</style><link rel="stylesheet" href="styles.dd0601ea4f6a73f4.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles.dd0601ea4f6a73f4.css"></noscript></head>
+<body>
+  <app-root></app-root>
+<script src="runtime.330379824b0935c8.js" type="module"></script><script src="polyfills.84549e991085cbaa.js" type="module"></script><script src="main.ec08fc1fb1d9dfbf.js" type="module"></script>
+
+</body></html>
